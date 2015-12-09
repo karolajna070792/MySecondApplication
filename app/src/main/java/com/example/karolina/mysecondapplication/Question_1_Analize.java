33696b51a2128a878bc7b022cef160203a04b029
karolajna070792 <karolajna070792@gmail.com>
2015-12-09 20:13:51 +0100

    improved - one background and one textColor

--- app/src/main/java/com/example/karolina/mysecondapplication/Question_1_Analize.java
+++ app/src/main/java/com/example/karolina/mysecondapplication/Question_1_Analize.java
@@ -27,11 +27,7 @@ public class Question_1_Analize extends AppCompatActivity {
         Button zamek_czocha = (Button) findViewById(R.id.zamek_czocha_btn);
 
         zamek_czocha.setTextColor(0xffffff00);
-
-        Button zamek_ksiaz = (Button) findViewById(R.id.zamek_ksiaz_btn);
-
-        zamek_ksiaz.setTextColor(0xff0000ff);
-
+        
         Button next_odp = (Button) findViewById(R.id.nastepna_odp_btn);
 
         Button good_answer = (Button) findViewById(R.id.palac_moszna_btn);
